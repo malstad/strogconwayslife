c0b5e4f7a5edb8d351bc08f1813f9e762511594d
StrogAnon <StrogAnon@0dcaee73-6038-51da-2fbd-95be92e50b49>
2010-11-23 17:59:56 +0000

    *Setup the project with SDL *Pasted in what code was already completed
    *TODO - Work on mouse input

---
+++ John Conway's Life/main.cpp
@@ -0,0 +1,211 @@
+#include <stdlib.h>
+#include <iostream>
+
+#include "SDL.h"
+#include "SDL_image.h"
+
+//globals
+bool*** g_grids;						// will be used as two, two-dimensional array of bools
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int SCREEN_BPP = 32;
+const int cellHeight = 10;				// height of each cell on the grid
+const int cellWidth = 11;				// wid                    th of each cell on the gridd
+const int nBuffers = 2;					// number of buffers
+int nRows = 50;							// number of rows
+int nColumns = 63;						// number of columns
+SDL_Rect g_srGrid;						// display grid
+SDL_Surface* g_ssScreen = NULL;			// screen used for displaying
+SDL_Surface* g_ssSpriteSheet = NULL;	// surface holding the sprite sheet
+SDL_Event g_seEvent;					// event handler
+
+void SetupGrid();
+void DrawGrid();
+
+//SDL related functions
+bool init();
+SDL_Surface* loadImage(std::string, SDL_Color);
+bool loadFiles();
+void applySurface(int, int, SDL_Surface*, SDL_Surface*, SDL_Rect*);
+void cleanup();
+
+int main(int argc, char** argv)
+{
+	if(!init())
+		return 1;
+	if(!loadFiles())
+		return 1;
+
+	bool toQuit = false;
+
+	SetupGrid();
+	DrawGrid();
+
+	SDL_Flip(g_ssScreen);
+
+	while(!toQuit)
+	{
+		while(SDL_PollEvent(&g_seEvent))
+		{
+			if(g_seEvent.type == SDL_QUIT)
+			{
+				toQuit = true;
+			}
+		}
+
+		//update stuff goes here
+	}
+
+	cleanup();
+	return 0;
+}
+
+void SetupGrid()
+{
+	//allocate the array
+	g_grids = new bool**[nBuffers];	//creates an array of length nBuffers of pointers to pointers to bools
+	for(int b = 0; b < nBuffers; b++)
+	{
+		g_grids[b] = new bool*[nRows];	//creates an array of length nRows of pointers to bools
+		for(int r = 0; r < nRows; r++)
+		{
+			g_grids[b][r] = new bool[nColumns];	//creates an array of length nColumns of bools
+		}
+	}
+
+	for(int b = 0; b < nBuffers; b++)
+	{
+		for(int r = 0; r < nRows; r++)
+		{
+			for(int c = 0; c < nColumns; c++)
+			{
+				g_grids[b][r][c] = false;		//initialize all bools on both grids to be false
+			}
+		}
+	}
+
+	// setup the visible grid
+	g_srGrid.x = 0;
+	g_srGrid.y = 0;
+	g_srGrid.w = SCREEN_WIDTH;
+	g_srGrid.h = SCREEN_HEIGHT;
+
+}
+
+void DrawGrid()
+{
+	SDL_Rect offset, dead, alive, currentClip;
+	offset.x = 50;
+	offset.y = 20;
+	dead.x = 0;
+	dead.y = 0;
+	dead.w = cellWidth;
+	dead.h = cellHeight;
+
+	alive.x = 12;
+	alive.y = 0;
+	alive.w = cellWidth;
+	alive.h = cellHeight;
+
+	for(int r = 0; r < nRows; r++)
+	{
+		for(int c = 0; c < nColumns; c++)
+		{
+			//determine what color to fill the current cell with
+			if(g_grids[0][r][c])
+				currentClip = alive;
+			else 
+				currentClip = dead;
+
+			//apply the current cell to the screen
+			applySurface(offset.x, offset.y, g_ssSpriteSheet, g_ssScreen, &currentClip);
+
+			//increment horizontal offset
+			offset.x += cellWidth;
+		}
+		offset.x = 50;			 // after drawing a horizontal line, reset horizontal offset
+		offset.y += cellHeight; // increment vertical offset
+	}
+
+}
+
+bool init()
+{
+	//initialize all subsytems
+	if(-1 == SDL_Init(SDL_INIT_EVERYTHING))
+		return false;
+
+	//initialize screen
+	g_ssScreen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+	if(NULL == g_ssScreen)	//if there was an error, return false
+		return false;
+
+	SDL_WM_SetCaption("John Conway's Game of Life", NULL);
+
+	return true;
+}
+
+SDL_Surface* loadImage(std::string sFileName, SDL_Color* key)
+{
+	SDL_Surface* loadedImage;
+	SDL_Surface* optimizedImage;
+
+	loadedImage = IMG_Load(sFileName.c_str());
+
+	if(NULL != loadedImage)
+	{
+		optimizedImage = SDL_DisplayFormat(loadedImage);
+		SDL_FreeSurface(loadedImage);
+
+		//sets color key as long as optimizing the image didn't fail and user didn't pass a NULL value for key
+		if(NULL != optimizedImage && NULL != key)
+		{
+			Uint32 colorkey = SDL_MapRGB(optimizedImage->format, key->r, key->g, key->b);
+			SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY, colorkey);
+		}
+	}
+
+	return optimizedImage;
+}
+
+bool loadFiles()		// uses loadImage
+{
+	g_ssSpriteSheet = loadImage("sprites.png", NULL);	//load sprites
+
+	//check if process failed
+	if(NULL == g_ssSpriteSheet)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+void applySurface(int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip)
+{
+	SDL_Rect offset;
+	offset.x = x;
+	offset.y = y;
+
+	SDL_BlitSurface(source, clip, destination, &offset);
+}
+
+void cleanup()
+{
+	for(int b = 0; b < nBuffers; b++)
+	{
+		for(int r = 0; r < nRows; r++)
+		{
+			delete [] g_grids[b][r];
+		}
+
+		delete [] g_grids[b];
+	}
+
+	delete [] g_grids;
+
+	SDL_FreeSurface(g_ssSpriteSheet);
+	SDL_Quit();							// quit SDL
+
+}
