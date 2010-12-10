aa1dc710bae1317ac1a2e1b07aba2c0d4e014175
StrogAnon <StrogAnon@0dcaee73-6038-51da-2fbd-95be92e50b49>
2010-12-10 05:02:52 +0000

    Working version! Still some thing to do though: *Make it so user can hold down left mouse and it still activates or deactivates cells *Have a display in the corner that shows what generation it's currently at *Have a drop down menu that determines how fast the simulation runs *Probably something other things, but that's all I can think of.
    ~I'm a happy, happy man at this moment.

--- trunk/John Conway's Life/Grid.h
+++ trunk/John Conway's Life/Grid.h
@@ -2,13 +2,14 @@
 #define GRID_H
 
 #include "MySDLFunctions.h"
+#include <Windows.h>
 
 class Grid
 {
 protected:
 	int m_nBuffers;						// number of buffers the grid will use
-	int m_DrawBuffer;					// variable holding the value of what buffer will be drawn to screen
-	int m_LogicBuffer;					// variable holding the value of what buffer the logic will be written to
+	int m_iDrawBuffer;					// variable holding the value of what buffer will be drawn to screen
+	int m_iLogicBuffer;					// variable holding the value of what buffer the logic will be written to
 	int m_nRows;						// number of rows the grid will have
 	int m_nColumns;						// number of coulumns the grid will have
 	int m_cellWidth;					// width of a cell in the grid
@@ -16,10 +17,12 @@ protected:
 	
 	bool*** m_grids;					// three dimensional array that stores bools
 	SDL_Rect m_srDisplayGrid;			// rect that will hold the display
-	SDL_Surface* m_ssDisplaySurface;	// display picture
-	SDL_Surface* m_ssCellSprites;		// surface holding the sprite sheet of a cell
-	SDL_Rect m_srDisplayOffset;			// how much the display rect will be offset
+	SDL_Surface* m_ssCellSprites;		// surface holding the sprite sheet of a single cell
+	SDL_Rect m_srDisplayOffset;			// how much the grid will be offset
 	SDL_Rect m_srCellSpecs;				// dimensions of a single cell
+
+	//functions only used by Grid class
+	int findCellNeighbors(int xPos, int yPos);	// determines the number of neighbors a given cell has on the grid
 
 public:
 	Grid();
@@ -27,6 +30,7 @@ public:
 	void Draw(SDL_Surface* Screen);
 	bool HandleMouseInput(float x, float y);
 	void Update();
+	bool CheckCell(int sourceBuffer, int r, int c);
 	//get functions
 	SDL_Rect getCellSpecs() { return m_srCellSpecs; }
 	SDL_Rect getMainRectOffset() { return m_srDisplayOffset; }
